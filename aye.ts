9322e2486fc1c3c7dbc095e080a4b637b8285d9f
Жарский Данила <DanilaZ@stud.vvsu.ru>
2024-02-06 16:27:03 +1000

    123

---
+++ aye.ts
@@ -0,0 +1,8 @@
+// class Class {}
+// class Point {x: number; y: number;}
+// // let point: Point;
+// let point: Point = new Point();
+// point.x = 1; point.y =2; 
+class Point3D {x: number; y: number; z: number;}
+let move3D: Point3D = new Point3D()
+move3D.x = 1; move3D.y = 2; move3D.z = 3;
